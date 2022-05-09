75f316227f474610b16f8e53488ba41fe0d2fb96
lancelot1672@naver.com <lancelot1672@naver.com>
2022-05-09 11:11:45 +0900

    Cat 클래스 추가

---
+++ src/com/kh/animal/Cat.java
@@ -0,0 +1,12 @@
+package com.kh.animal;
+
+public class Cat implements Animal{
+	public static void main(String[] args) {
+		Cat c1 = new Cat();
+		c1.bark();
+	}
+
+	@Override
+	public void bark() {
+	}
+}
