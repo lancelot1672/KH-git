5f140aaf83093baa24a840fd58ee490cd8621c17
lancelot1672@naver.com <lancelot1672@naver.com>
2022-05-09 13:58:59 +0900

    Cow#type 추가

--- src/com/kh/animal/Cow.java
+++ src/com/kh/animal/Cow.java
@@ -1,5 +1,17 @@
 package com.kh.animal;
 
 public class Cow {
+	/**
+	 * reset : 지정버전으로 돌아감
+	 *  - hard : 이후  변경사항 모두 제거
+	 */
+	
+	/**
+	 * revert : 지정한 버전의 취소버전을 새로 추가
+	 * - history를 변경할 수 없는 경우 유용하다.
+	 * - conflict 발생가능성 있음.
+	 */
 	private String name;
+	
+	
 }
